f060c4e00464d84a309affa9dc9c46ddc021a6f7
Takuro Fukamizu <takuro.f.1987@gmail.com>
2020-09-22 11:28:13 +0900

    fix typo

--- cage-garde-control-m5atom/Weather.h
+++ cage-garde-control-m5atom/Weather.h
@@ -5,6 +5,6 @@ public:
     Weather(String apiKey);
     bool update();
 private:
-    const static String endpoint = "http://api.openweathermap.org/data/2.5/weather?q=tokyo,jp&APPID=";
+    const String endpoint = "http://api.openweathermap.org/data/2.5/weather?q=tokyo,jp&APPID=";
     String _apiKey;
 };
